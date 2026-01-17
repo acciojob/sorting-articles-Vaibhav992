cb70a079ffd5fb96ccdd23b43b8adcc59ec82010
Vaibhav Sharma <92776886+Vaibhav992@users.noreply.github.com>
2026-01-17 19:16:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+const bands = [
+	'The Plot in You',
+  'The Devil Wears Prada',
+  'Pierce the Veil',
+  'Norma Jean',
+  'The Bled',
+  'Say Anything',
+  'The Midway State',
+  'We Came as Romans',
+  'Counterparts',
+  'Oh, Sleeper',
+  'A Skylit Drive',
+  'Anywhere But Here',
+  'An Old Dog'
+]
+function stripArticle(name) {
+  return name.replace(/^(a |an |the )/i, '').trim();
+}
+
+const sortedBands = bands.sort((a, b) => {
+  const bandA = stripArticle(a).toLowerCase();
+  const bandB = stripArticle(b).toLowerCase();
+
+  if (bandA < bandB) return -1;
+  if (bandA > bandB) return 1;
+  return 0;
+});
+const ul = document.getElementById('band');
+
+sortedBands.forEach(band => {
+  const li = document.createElement('li');
+  li.textContent = band;
+  ul.appendChild(li);
+});
+
+
